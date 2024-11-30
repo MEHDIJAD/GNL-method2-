92ede2aabfb8d89095b28219346864857ff60f35
MEHDIJAD <elmehdielgarouaz@gmail.com>
2024-11-30 17:04:46 +0100

    fix

--- get_next_line.c
+++ get_next_line.c
@@ -17,26 +17,22 @@ int ft_found_newline(char *str_buf)
 }
 int ft_len_to_newline(char *str_buf)
 {
-    int i;
     int len;
 
     if (!str_buf)
         return (0);
-    i = 0;
     len = 0;
-    while (str_buf[i])
-    {
-        if (str_buf[i] == '\n')
-            return (++len);
-        i++;
-        len++;  
-    }
+    while (str_buf[len] && str_buf[len] != '\n')
+        len++;
+    if (str_buf[len] == '\n')
+        len++;
     return (len);
 }
 char *ft_get_line(char * str_buf)
 {
     int len;
     char *line;
+
     if (!str_buf)
         return (NULL);
     len = ft_len_to_newline(str_buf);
@@ -44,7 +40,6 @@ char *ft_get_line(char * str_buf)
     if (!line)
         return (NULL);
     ft_strlcpy(line, str_buf, len + 1);
-    free(str_buf); //!! end wiht str_buff
     return (line);
 }
 
@@ -60,7 +55,7 @@ void ft_read_join(char **str_buff, int fd)
         if (!buf)
             return ;
         bytes_read = read(fd, buf, BUFFER_SIZE);
-        if (!bytes_read)
+        if (bytes_read < 0)
         {
             free(buf); //!! in nothing is read
             return ;
@@ -70,6 +65,8 @@ void ft_read_join(char **str_buff, int fd)
         free(*str_buff);
         *str_buff = temp;
         free(buf); //!! end with buff
+        if (bytes_read == 0)
+            break;
     }
 }
 
@@ -77,18 +74,21 @@ char *get_next_line(int fd)
 {
     static char *str_buf;
     char *line;
+    char *temp;
+
     //* checks: if fd = -1 or if nothing to read or if no access to read 
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
         return (NULL);
     ft_read_join(&str_buf, fd);
-    if (!str_buf)
-        return (free(str_buf), NULL); //!!
-    line = ft_get_line(str_buf);
-    str_buf = ft_strdup(str_buf + ft_len_to_newline(str_buf));
-    if (!str_buf[0])
+    if (!str_buf || !*str_buf)
     {
-        free (str_buf);
+        free(str_buf);
         str_buf = NULL;
+        return (NULL);
     }
+    line = ft_get_line(str_buf);
+    temp = ft_strdup(str_buf + ft_len_to_newline(str_buf));
+    free(str_buf);
+    str_buf = temp;
     return (line);
 }
